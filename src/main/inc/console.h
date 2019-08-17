469ceff05f4c3ce967388eb83077808c9ad75bab
Jacob Walser <jwalser90@gmail.com>
2019-08-17 00:22:17 -0400

    console: og implementation

---
+++ src/main/inc/console.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <inttypes.h>
+
+void console_initialize();
+void console_write(const char* string);
+void console_write_int(uint32_t i);
---
+++ src/main/src/console.c
@@ -0,0 +1,32 @@
+#include <console.h>
+
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/usart.h>
+
+#include <target.h>
+
+#include <usart.h>
+
+void console_initialize() {
+    rcc_periph_clock_enable(CONSOLE_TX_GPIO_RCC);
+
+    gpio_mode_setup(CONSOLE_TX_GPIO_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, CONSOLE_TX_GPIO_PIN);
+
+    gpio_set_af(CONSOLE_TX_GPIO_PORT, CONSOLE_TX_GPIO_AF, CONSOLE_TX_GPIO_PIN);
+
+    rcc_periph_clock_enable(CONSOLE_USART_RCC);
+
+    // higher clock speed setup necessary for faster baudrates
+    usart_initialize(CONSOLE_USART, 9600);
+}
+
+void console_write(const char* string)
+{
+   usart_write(CONSOLE_USART, string);
+}
+
+void console_write_int(const uint32_t i)
+{
+    usart_write_int(CONSOLE_USART, i);
+}
