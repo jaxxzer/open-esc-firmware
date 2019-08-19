678508b8a573cf1a85e9b5c8a75687765d1cb0ce
Jacob Walser <jwalser90@gmail.com>
2019-08-19 01:20:23 -0400

    hal: update pwm-input

--- src/hal/inc/pwm-input.h
+++ src/hal/inc/pwm-input.h
@@ -3,13 +3,26 @@
 #include <inttypes.h>
 #include <stdbool.h>
 
+#define PERIOD_MAX_NS 22000000 // 50Hz - 10%
+
+// dshot150 is 150kbps
+#define DIGITAL_PERIOD_MAX_NS 28409 // ie 'bits' incoming faster than 32kHz + 10%
+
+#define MULTISHOT_DUTY_MAX_NS 27500// 25us + 10%
+#define ONESHOT42_DUTY_MAX_NS 92400 // 84us + 10%
+#define ONESHOT125_DUTY_MAX_NS 275000 // 250us + 10%
+#define CONVENTIONAL_DUTY_MAX_NS 2200000 // 2000us + 10%
+
 typedef enum {
+    PWM_INPUT_TYPE_NONE,
     PWM_INPUT_TYPE_CONVENTIONAL,
     PWM_INPUT_TYPE_ONESHOT125,
     PWM_INPUT_TYPE_ONESHOT42,
     PWM_INPUT_TYPE_MULTISHOT,
     PWM_INPUT_TYPE_UNKNOWN,
 } pwm_input_type_t;
+
+pwm_input_type_t g_pwm_type;
 
 void pwm_input_initialize();
 void pwm_input_deinitialize();
@@ -20,15 +33,20 @@ uint32_t pwm_input_get_timeout();
 
 pwm_input_type_t pwm_input_detect_type();
 
+uint32_t pwm_input_tick_period_ns();
+
 // period in ticks
 uint32_t pwm_input_get_period();
+uint32_t pwm_input_get_period_ns();
 
 // duty in ticks
 uint32_t pwm_input_get_duty();
+uint32_t pwm_input_get_duty_ns();
 
 void pwm_input_set_type(pwm_input_type_t input_type);
-pwm_input_type_t pwm_input_get_type();
 
+// 16-bit throttle
+uint16_t pwm_input_get_throttle();
 
 // still valid?
 // valid if:
--- src/hal/src/stm32/common/pwm-input.c
+++ src/hal/src/stm32/common/pwm-input.c
@@ -6,8 +6,13 @@
 
 #include <target.h>
 
+// Must be a high resolution timer!
+#if PWM_INPUT_TIMER != TIM2
+ #error PWM_INPUT_TIMER must be a high resolution 32 bit timer
+#endif
+
 static uint16_t psc = 0;
-static uint16_t arr = 0xffff;
+static uint32_t arr = 0;
 
 void pwm_input_initialize()
 {
@@ -15,9 +20,11 @@ void pwm_input_initialize()
   gpio_mode_setup(PWM_INPUT_GPIO_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, PWM_INPUT_GPIO_PIN);
   gpio_set_af(PWM_INPUT_GPIO_PORT, PWM_INPUT_GPIO_AF, PWM_INPUT_GPIO_PIN);
 
-  // set timer prescaler so that timer counter runs at 1MHz
-  // the pwm duty counter then measures pulse length in microseconds
-  psc = (rcc_ahb_frequency / 1000000) - 1;
+  // set timer prescaler so that timer counter runs at 8MHz
+  psc = (rcc_ahb_frequency / 8000000) - 1;
+
+  // arr = period / tick time
+  arr = PERIOD_MAX_NS / pwm_input_tick_period_ns();
 
   // trigger input 1 will:
   // 1. capture the signal period in CCR1 (rising edge - rising edge time)
@@ -44,7 +51,93 @@ void pwm_input_initialize()
   timer_enable_counter(PWM_INPUT_TIMER); // set CEN
 }
 
+pwm_input_type_t pwm_input_detect_type()
+{
+  uint32_t duty = pwm_input_get_duty_ns();
+  uint32_t period = pwm_input_get_period_ns();
+
+  // if the signal frequency is > 32kHz, it's a digital protocol
+  // if the signal period is < 1/32kHz = 31250 nanoseconds, it's a digital protocol
+  if (period == 0) {
+    return PWM_INPUT_TYPE_NONE;
+  } else if (period < DIGITAL_PERIOD_MAX_NS) {
+    return PWM_INPUT_TYPE_UNKNOWN; // no support for dshot or multishot yet
+  } else {
+  // otherwise, it's an 'analog' pwm protocol
+  // we can determine thdutye type now by looking at the duty
+    if (duty == 0) {
+      return PWM_INPUT_TYPE_NONE;
+    } else if (duty < MULTISHOT_DUTY_MAX_NS) {
+      return PWM_INPUT_TYPE_MULTISHOT;
+    } else if (duty < ONESHOT42_DUTY_MAX_NS) {
+      return PWM_INPUT_TYPE_ONESHOT42;
+    } else if (duty < ONESHOT125_DUTY_MAX_NS) {
+      return PWM_INPUT_TYPE_ONESHOT125;
+    } else if (duty < CONVENTIONAL_DUTY_MAX_NS) {
+      return PWM_INPUT_TYPE_CONVENTIONAL;
+    } else {
+      return PWM_INPUT_TYPE_UNKNOWN;
+    }
+  }
+}
+
+uint32_t pwm_input_tick_period_ns()
+{
+  // 1e9 / clock frequency
+  return 1000000000 / (rcc_ahb_frequency / (psc + 1));
+}
+
+uint32_t pwm_input_get_period()
+{
+  return PWM_INPUT_PERIOD_CCR(PWM_INPUT_TIMER);
+}
+
+uint32_t pwm_input_get_period_ns()
+{
+  // tick counts * tick period in nanoseconds
+  return pwm_input_get_period() * pwm_input_tick_period_ns();
+}
+
 uint32_t pwm_input_get_duty()
 {
   return PWM_INPUT_DUTY_CCR(PWM_INPUT_TIMER);
+}
+
+uint32_t pwm_input_get_duty_ns()
+{
+  // tick counts * tick period in nanoseconds
+  return pwm_input_get_duty() * pwm_input_tick_period_ns();
+}
+
+uint16_t pwm_input_throttle_scale_ns(uint32_t duty_min_ns, uint32_t duty_max_ns)
+{
+  uint32_t duty = pwm_input_get_duty_ns();
+
+  if (duty < duty_min_ns) {
+    return 0;
+  }
+
+  if (duty > duty_max_ns) {
+    return 0xffff;
+  }
+
+  return ((0xffff * (uint64_t)(duty - duty_min_ns))) / ((duty_max_ns - duty_min_ns));
+}
+
+uint16_t pwm_input_get_throttle()
+{
+  switch(g_pwm_type) {
+    case PWM_INPUT_TYPE_CONVENTIONAL:
+      return pwm_input_throttle_scale_ns(1000000, 2000000);
+    case PWM_INPUT_TYPE_ONESHOT125:
+      return pwm_input_throttle_scale_ns(125000, 250000);
+    case PWM_INPUT_TYPE_ONESHOT42:
+      return pwm_input_throttle_scale_ns(42000, 84000);
+    case PWM_INPUT_TYPE_MULTISHOT:
+      return pwm_input_throttle_scale_ns(5000, 25000);
+    case PWM_INPUT_TYPE_NONE:
+    case PWM_INPUT_TYPE_UNKNOWN:
+    default:
+      return 0;
+  }
 }
