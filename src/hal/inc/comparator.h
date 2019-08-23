c5a8386492c3e58cead2a80a1cb292751b790e46
Jacob Walser <jwalser90@gmail.com>
2019-08-23 00:54:12 -0400

    hal: add comparator

---
+++ src/hal/inc/comparator.h
@@ -0,0 +1,28 @@
+// single comparator driver
+
+#pragma once
+
+#include <inttypes.h>
+
+typedef enum {
+    COMP_STATE0,
+    COMP_STATE1,
+    COMP_STATE2,
+    COMP_STATE3,
+    COMP_STATE4,
+    COMP_STATE5,
+} comp_state_e;
+
+comp_state_e g_comparator_state;
+
+void comparator_initialize();
+
+void comparator_set_state(comp_state_e new_state);
+
+void comparator_zc_isr_enable();
+void comparator_zc_isr_disable();
+
+// comparator bemf zero cross isr, normally you will commutate
+void comparator_zc_isr();
+
+void comparator_blank(uint32_t nanoseconds);
---
+++ src/hal/src/stm32/common/comparator.c
@@ -0,0 +1,98 @@
+#include <comparator.h>
+
+#include <libopencm3/cm3/assert.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/vector.h>
+#include <libopencm3/stm32/comparator.h>
+#include <libopencm3/stm32/exti.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/timer.h>
+
+#include <target.h>
+
+volatile uint16_t comparator_states[6] = {
+    0x041,
+    0x861,
+    0x061,
+    0x851,
+    0x051,
+    0x841,
+};
+
+uint32_t comparator_blank_tick_period_ns;
+
+// TODO move nvic isrs to target directory
+
+// linked for f0
+void adc_comp_isr() {
+    comparator_zc_isr(); // user code
+    exti_reset_request(EXTI21);
+}
+
+// linked for f3
+void comp123_isr() {
+    comparator_zc_isr();
+    exti_reset_request(EXTI21);
+}
+
+void comparator_blank_complete_isr()
+{
+    comparator_zc_isr_enable();
+}
+
+void tim17_isr() {
+    comparator_blank_complete_isr();
+    timer_clear_flag(TIM17, TIM_SR_UIF);
+}
+
+void comparator_initialize()
+{
+    rcc_periph_clock_enable(RCC_GPIOA);
+
+    gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO0 | GPIO1 | GPIO4 | GPIO5);
+
+    rcc_periph_clock_enable(RCC_SYSCFG_COMP);
+
+    comp_select_hyst(1, COMP_CSR_HYST_HIGH);
+
+    // enable comparator (there is a startup delay)
+    comparator_set_state(COMP_STATE1);
+
+    exti_set_trigger(EXTI21, EXTI_TRIGGER_BOTH);
+
+    rcc_periph_clock_enable(COMPARATOR_BLANK_TIMER_RCC);
+
+    nvic_enable_irq(COMPARATOR_BLANK_IRQ);
+    timer_one_shot_mode(COMPARATOR_BLANK_TIMER);
+    timer_enable_irq(COMPARATOR_BLANK_TIMER, TIM_DIER_UIE);
+
+    comparator_blank_tick_period_ns = 1000000000 / (rcc_ahb_frequency / (TIM_PSC(COMPARATOR_BLANK_TIMER) + 1));
+}
+
+void comparator_set_state(comp_state_e new_state)
+{
+    cm3_assert(new_state <= COMP_STATES);
+
+    g_comparator_state = new_state;
+    COMP_CSR(COMP1) |= comparator_states[g_comparator_state];
+}
+
+void comparator_zc_isr_enable()
+{
+    exti_enable_request(EXTI21);
+    nvic_enable_irq(COMPARATOR_ZC_IRQ);
+}
+
+void comparator_zc_isr_disable()
+{
+    nvic_disable_irq(COMPARATOR_ZC_IRQ);
+    exti_disable_request(EXTI21);
+}
+
+void comparator_blank(uint32_t nanoseconds)
+{
+    comparator_zc_isr_disable();
+    timer_set_period(COMPARATOR_BLANK_TIMER, nanoseconds / comparator_blank_tick_period_ns);
+    timer_enable_counter(COMPARATOR_BLANK_TIMER);
+}
