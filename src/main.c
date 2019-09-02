4eeaa7c5bdd0575ec3a5c7a2493a3b3647426cdd
Jacob Walser <jwalser90@gmail.com>
2019-09-01 22:12:00 -0400

    main: wip

--- src/main.c
+++ src/main.c
@@ -3,6 +3,10 @@
 #include <console.h>
 #include <pwm-input.h>
 
+#include "debug.h"
+
+#include <libopencm3/cm3/vector.h>
+#include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/comparator.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
@@ -10,78 +14,213 @@
 
 #include <target.h>
 
-const uint16_t comp_checks_required = 5;
-volatile uint16_t comp_checks;
+volatile bool starting;
 
-void comparator_zc_isr()
-{
-  while (comp_checks < comp_checks_required) {
-    if (!(COMP_CSR(COMP1) & COMP_CSR_OUT)) {
-      comp_checks = 0;
-      return;
-    } else {
-      comp_checks++;
-    }
+uint32_t zc_confirmations_required = 20;
+const int16_t tim15psc = 10;
+volatile uint32_t zc_counter;
+volatile uint8_t advance = 0;
+
+const uint16_t defaultCCR1 = 20000;
+const uint16_t defaultCCR2 = defaultCCR1 + defaultCCR1/4;
+
+// void led_toggle() {
+//   gpio_toggle(LED_GPIO_PORT, LED_GPIO_PIN);
+// }
+
+void tim15_isr() {
+  // check tim15 cc1 interrupt
+  if (timer_get_flag(TIM15, TIM_SR_UIF)) {
+
+    debug1_toggle();
+    // starting = true;
+    // TIM_CCR1(TIM15) = defaultCCR1;
+    // TIM_CCR2(TIM15) = defaultCCR2;
+    // comparator_zc_isr_disable();
+    timer_clear_flag(TIM15, TIM_SR_UIF);
   }
 
-  comp_checks = 0;
+  if (timer_get_flag(TIM15, TIM_SR_CC1IF)) {
 
-  // blank for 1ms
+    debug1_toggle();
+    debug1_toggle();
+    bridge_commutate();
+    // TIM_CCR1(TIM15) *= 4;
+
+    comparator_set_state(g_bridge_comm_step + 2);
+    timer_clear_flag(TIM15, TIM_SR_CC1IF);
+  }
+  // check tim15 cc2 interrupt
+  // ccr2 - ccr1 = blanking period
+  if (timer_get_flag(TIM15, TIM_SR_CC2IF)) {
+    zc_counter = zc_confirmations_required;
+    comparator_zc_isr_enable();
+    timer_clear_flag(TIM15, TIM_SR_CC2IF);
+  }
+}
+
+const uint16_t zc_required = 2;
+volatile uint16_t zc_pass;
+// for test
+void comparator_zc_isr()
+{
+  while (zc_pass++ < zc_required) {
+    if (!(COMP_CSR(COMP1) & COMP_CSR_OUT)) {
+      zc_pass = 0;
+      return;
+    }
+  }
   comparator_blank(50000);
   comparator_set_state(g_comparator_state + 1);
   gpio_toggle(LED_GPIO_PORT, LED_GPIO_PIN);
+  zc_pass = 0;
+}
+
+
+// void comparator_zc_isr()
+// {
+//   if (TIM_CNT(TIM1) < 120) {
+//     return;
+//   }
+
+//   while (zc_counter--) {
+//     if (!(COMP_CSR(COMP1) & COMP_CSR_OUT)) {
+//       zc_counter = zc_confirmations_required;
+//       return;
+//     } 
+//   }
+
+//   // TIM_CR1(TIM15) &= ~TIM_CR1_CEN; // disable counter
+
+//   volatile uint16_t cnt = TIM_CNT(TIM15);
+//   TIM_CNT(TIM15) = 1;
+
+
+
+//   if (starting) {
+//     starting = false;
+//     debug2_toggle();
+//     debug2_toggle();
+
+//     TIM_CCR1(TIM15) = cnt;
+//   } else {
+//     if (cnt < TIM_CCR1(TIM15)) {
+//       starting = true;
+//       debug0_toggle();
+//       debug0_toggle();
+//       TIM_CCR1(TIM15) = defaultCCR1;
+//     } else {
+//       debug2_toggle();
+//       TIM_CCR1(TIM15) = cnt/2;
+//     }
+//   }
+
+//   TIM_CCR2(TIM15) = TIM_CCR1(TIM15) + TIM_CCR1(TIM15)/8;
+
+//   // TIM_CCR2(TIM15) = 800;
+//   comparator_zc_isr_disable();
+//   zc_counter = zc_confirmations_required;
+//   // TIM_CR1(TIM15) |= TIM_CR1_CEN; // disable counter
+// }
+
+void commutation_timer_enable_interrupts()
+{
+  nvic_enable_irq(NVIC_TIM15_IRQ);
+  TIM_DIER(TIM15) |= TIM_DIER_CC1IE | TIM_DIER_CC2IE | TIM_DIER_UIE;
+}
+
+void commutation_timer_disable_interrupts()
+{
+  TIM_DIER(TIM15) &= ~(TIM_DIER_CC1IE | TIM_DIER_CC2IE);
+  nvic_disable_irq(NVIC_TIM15_IRQ);
+}
+
+void stop_motor()
+{
+  commutation_timer_disable_interrupts();
+  comparator_zc_isr_disable();
+}
+
+void start_motor()
+{
+  debug0_toggle();
+  starting = true;
+  TIM_CR1(TIM15) &= ~TIM_CR1_CEN; // disable counter
+  TIM_CNT(TIM15) = 1; // set counter to zero
+  TIM_CCR1(TIM15) = defaultCCR1;
+  TIM_CCR2(TIM15) = defaultCCR2;
+  zc_counter = zc_confirmations_required;
+
+  g_bridge_comm_step = BRIDGE_COMM_STEP0;
+  comparator_set_state(g_bridge_comm_step);
+
+  commutation_timer_enable_interrupts();
+  bridge_set_run_duty(90);
+  TIM_CR1(TIM15) |= TIM_CR1_CEN; // enable counter
+}
+
+void commutation_timer_setup()
+{
+  rcc_periph_clock_enable(RCC_TIM15);
+  TIM_ARR(TIM15) = 0xffff;
+
+  TIM_PSC(TIM15) = tim15psc;
+    TIM_EGR(TIM15) |= TIM_EGR_UG;
+    TIM_SR(TIM15) = 0;
 }
 
 int main()
 {
-  console_initialize();
-  console_write("\r\nWelcome to gsc: the gangster esc!\r\n");
+  debug_setup();
 
-  console_write("testing bridge...\r\n");
+  //while(1);
+  // bridge_initialize();
+  // bridge_enable();
+  // bridge_set_state(BRIDGE_STATE_AUDIO);
+  // bridge_set_audio_duty(0x1);
 
-  bridge_initialize();
-  bridge_enable();
-  bridge_set_state(BRIDGE_STATE_AUDIO);
-  bridge_set_audio_duty(0xf);
+  // for (int j = 0; j < 10; j++) {
+  // bridge_set_audio_frequency(8000);
+  // for (uint32_t i = 0; i < 5000; i++) { float a = 0.6*9; }
+  // bridge_set_audio_frequency(3000);
+  // for (uint32_t i = 0; i < 10000; i++) { float a = 0.6*9; }
+  // }
+  // bridge_set_audio_frequency(3500);
+  // for (uint32_t i = 0; i < 60000; i++) { float a = 0.6*9; }
 
-  bridge_set_audio_frequency(800);
-  for (int i = 0; i < 9999; i++) { i -= 1; float a = 0.6*9;i += 1; }
-  bridge_set_audio_frequency(2000);
-  for (int i = 0; i < 9999; i++) { float a = 0.6*9; }
-  bridge_set_audio_frequency(3500);
-  for (int i = 0; i < 9999; i++) { float a = 0.6*9; }
+  // bridge_disable();
 
-  bridge_set_state(BRIDGE_STATE_RUN);
-
-  for (int i = 0; i < 12; i++) {
-    bridge_commutate();
-    for (int i = 0; i < 400; i++)
-    {
-      bridge_set_run_duty(i);
-    }
-  }
-
-  bridge_disable();
-
-  for (int i = 0; i < 9999; i++) { float a = 0.6*9; }
-
-  console_write("initializing comparator...\r\n");
+  // for (int i = 0; i < 9999; i++) { float a = 0.6*9; }
 
   comparator_initialize();
+
+  // for test
   comparator_zc_isr_enable();
+  while(1);
 
-  pwm_input_initialize();
+  commutation_timer_setup();
+  bridge_enable();
+  bridge_set_state(BRIDGE_STATE_RUN);
+  for (uint8_t i = 0; i < 6; i++) {
+    start_motor();
+    advance = i;
+    for (uint32_t i = 0; i < 400000; i++) { float a = 0.6*9; }
+    stop_motor();
+  }
+  bridge_disable();
+  stop_motor();
 
-  rcc_periph_clock_enable(LED_GPIO_RCC);
+  while(1);
 
-  gpio_mode_setup(LED_GPIO_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_GPIO_PIN);
 
-  console_write("waiting for pwm input...\r\n");
+  //console_write("waiting for pwm input...\r\n");
+
 
   uint8_t succeded = 0;
   const uint8_t succeded_needed = 3;
 
   while (1) {
+    // gpio_toggle(LED_GPIO_PORT, LED_GPIO_PIN);
     pwm_input_type_t pwm_type_last = g_pwm_type;
     while (!timer_get_flag(PWM_INPUT_TIMER, TIM_SR_UIF)) {
     }
