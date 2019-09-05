34798fbbf2e072ac9ea9e6244837cbe79740e6ad
Jacob Walser <jwalser90@gmail.com>
2019-09-05 00:02:32 -0400

    target: move zc timer to tim14
    all targets have this timer, as well as anticipated f031 (stspin)

--- src/target/inc/stm32/common/target.h
+++ src/target/inc/stm32/common/target.h
@@ -1,6 +1,6 @@
 #pragma once
 
 // requires at least one compare channel for comparator blanking
-#define ZC_TIMER TIM6
-#define ZC_TIMER_RCC RCC_TIM6
-#define ZC_TIMER_IRQ NVIC_TIM6_DAC_IRQ
+#define ZC_TIMER TIM14
+#define ZC_TIMER_RCC RCC_TIM14
+#define ZC_TIMER_IRQ NVIC_TIM14_IRQ
