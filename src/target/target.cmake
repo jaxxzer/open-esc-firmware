c2e2d579e5036f3bde113ec31ccc6bfe033ee222
Jacob Walser <jwalser90@gmail.com>
2019-09-10 23:43:50 -0400

    cmake: rework target cmake includes

---
+++ src/target/target.cmake
@@ -0,0 +1,39 @@
+# target board
+if(NOT DEFINED TARGET_BOARD)
+  set(TARGET_BOARD "wraith32")
+  message("no TARGET_BOARD defined, using ${TARGET_BOARD}")
+endif()
+
+# target board directory
+set(TARGET_DIR "${PROJECT_SOURCE_DIR}/src/target")
+
+# include target board CMakeLists
+include(${TARGET_DIR}/${TARGET_BOARD}/target-mcu.cmake)
+
+# target part
+# TODO fail if not set
+message("TARGET_MCU: ${TARGET_MCU}")
+
+include(${BOILERPLATE}/src/target-mcu.cmake)
+
+# target architecture
+# TODO fail if not set
+message("TARGET_CPU: ${TARGET_CPU}")
+
+# include target board defintions
+include_directories(${TARGET_DIR}/${TARGET_BOARD})
+
+if(TARGET_MCU MATCHES "STM32.*")
+    include_directories(${TARGET_DIR}/inc/stm32/common)
+    if(TARGET_MCU MATCHES "STM32F0.*")
+        include_directories(${TARGET_DIR}/inc/stm32/f0)
+        set(ISR_SRC ${TARGET_DIR}/inc/stm32/f0/isr.c)
+    elseif(TARGET_MCU MATCHES "STM32F3.*")
+        include_directories(${TARGET_DIR}/inc/stm32/f3)
+        set(ISR_SRC ${TARGET_DIR}/inc/stm32/f3/isr.c)
+    else()
+        message("unsupported TARGET_MCU: ${TARGET_MCU}")
+    endif()
+else()
+    message("unsupported TARGET_MCU: ${TARGET_MCU}")
+endif()
