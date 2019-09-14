739aaf36cd911a7bc2ce08acf16fc8bfbe026d8c
Jacob Walser <jwalser90@gmail.com>
2019-09-14 19:57:00 -0400

    isr: add usart dma transfer complete

--- src/main/inc/isr.h
+++ src/main/inc/isr.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <inttypes.h>
+
 void commutation_isr();
 void comparator_unblank_isr();
 void comparator_zc_isr();
 void comparator_zc_timeout_isr();
 void comparator_blank_complete_isr();
+void usart_dma_transfer_complete_isr(uint32_t usart);
