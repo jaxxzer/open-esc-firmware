d423a8c61e3ba76e706f832100d09d918ddb3de8
Jacob Walser <jwalser90@gmail.com>
2019-10-19 00:41:29 -0400

    wraith32: reduce wwdg current max

--- src/target/wraith32/target.h
+++ src/target/wraith32/target.h
@@ -120,6 +120,6 @@
                         BUS_CURRENT_GPIO_PIN
 
 // adc threshold for current channel
-#define ADC_WWDG_CURRENT_MAX 1300
+#define ADC_WWDG_CURRENT_MAX 200
 
 extern uint8_t adc_channels[7];
