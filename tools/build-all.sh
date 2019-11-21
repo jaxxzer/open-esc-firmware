6548af137a08d652659351d8b5a80b50f5f23edf
Jacob Walser <jwalser90@gmail.com>
2019-11-20 23:34:31 -0500

    tools: update arm toolchain to arm9

--- tools/build-all.sh
+++ tools/build-all.sh
@@ -4,7 +4,7 @@ source tools/travis-ci-functions.sh
 
 test tools/generate-nvic-all.sh
 
-test export PATH=$(pwd)/arm8/gcc-arm-none-eabi-8-2019-q3-update/bin:$PATH
+test export PATH=$(pwd)/arm9/gcc-arm-none-eabi-9-2019-q4-major/bin:$PATH
 
 mkdir -p build
 pushd build
--- tools/install-arm-toolchain.sh
+++ tools/install-arm-toolchain.sh
@@ -2,10 +2,10 @@
 
 source tools/travis-ci-functions.sh
 
-[ ! -d arm8 ] || exit 0
-mkdir -p arm8
-pushd arm8
-test wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/8-2019q3/gcc-arm-none-eabi-8-2019-q3-update-linux.tar.bz2
-test tar -xf gcc-arm-none-eabi-8-2019-q3-update-linux.tar.bz2
+[ ! -d arm9 ] || exit 0
+mkdir -p arm9
+pushd arm9
+test wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/RC2.1/gcc-arm-none-eabi-9-2019-q4-major-x86_64-linux.tar.bz2
+test tar -xf gcc-arm-none-eabi-9-2019-q4-major-x86_64-linux.tar.bz2
 popd
-test export PATH=$(pwd)/arm8/gcc-arm-none-eabi-8-2019-q3-update/bin:$PATH
+test export PATH=$(pwd)/arm9/gcc-arm-none-eabi-9-2019-q4-major/bin:$PATH
