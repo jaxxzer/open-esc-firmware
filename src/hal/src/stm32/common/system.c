cb0ce98d2877e6f45c35d961f85afedaa9777c1e
Jacob Walser <jwalser90@gmail.com>
2019-12-07 02:40:28 -0500

    system: add stm32g4

--- src/hal/src/stm32/common/system.c
+++ src/hal/src/stm32/common/system.c
@@ -13,5 +13,11 @@ void system_clock_initialize()
   rcc_clock_setup_in_hsi_out_48mhz();
 #elif defined(STM32F3)
   rcc_clock_setup_pll(&rcc_hsi_configs[1]);
+#elif defined(STM32G4)
+  rcc_clock_setup(&rcc_clock_config[RCC_CLOCK_CONFIG_HSI_PLL_170MHZ]);
+  // enable rcc peripheral clock for dmamux
+  *(uint32_t*)0x40021048 |= 0b100;
+#else
+#error "system not defined"
 #endif
 }
