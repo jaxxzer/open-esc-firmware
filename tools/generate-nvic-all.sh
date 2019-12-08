b781b2be3eb1b25db3b01af2ca21e1a193cac32e
Jacob Walser <jwalser90@gmail.com>
2019-12-07 22:55:02 -0500

    tools/generate-nvic: generate nvic for stm32g4

--- tools/generate-nvic-all.sh
+++ tools/generate-nvic-all.sh
@@ -2,7 +2,7 @@
 
 pushd lib/stm32-boilerplate/lib/libopencm3
 
-for PLATFORM in f0 f1 f3 g0
+for PLATFORM in f0 f1 f3 g0 g4
 do
   ./scripts/irq2nvic_h ./include/libopencm3/stm32/${PLATFORM}/irq.json
 done
